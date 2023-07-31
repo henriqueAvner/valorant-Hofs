13578eaa5c0797f7c881aa8e54e40a84b1e358e8
henriqueAvner <avnerhdpb@gmail.com>
2023-07-31 17:57:55 -0300

    Feat: Realizando a função getSentinelsHabilities

--- valorant_functions/getSentHabilities.js
+++ valorant_functions/getSentHabilities.js
@@ -1,1 +1,12 @@
-const objAgents = require('../valorant_functions/agents');
+const agents = require('../valorant_functions/agents');
+
+const getSentHabilities = () => {
+    return agents.filter((agent) => {
+        if (agent.classe === 'Sentinela') {
+            return agent;
+        }
+    }).map((agente) => {
+        return {nome: agente.name, habilidades: agente.habilidades} 
+    });
+}
+console.log(getSentHabilities());
