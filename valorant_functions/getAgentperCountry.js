a2e2a908be7fa69f1e108c13ffd9ae518f3ced6a
henriqueAvner <avnerhdpb@gmail.com>
2023-08-01 16:53:37 -0300

    Feat: Realizando a função getAgentperCountry

--- valorant_functions/getAgentperCountry.js
+++ valorant_functions/getAgentperCountry.js
@@ -1,1 +1,13 @@
-const objAgents = require('../valorant_functions/agents');
+const agents = require('../valorant_functions/agents');
+
+const getAgentperCountry = (country) => {
+    return agents.filter((objAgent) => {
+        if (objAgent.pais == country) {
+            return objAgent.name;
+        }
+    }).map((agent) => agent.name);
+}
+console.log(getAgentperCountry('Estados Unidos'));
+
+
+//Como eu queria trazer apenas o nome do agente, ao invés de utilizar o find junto com o map, resolvi utilizar o filter, pois o find não estava aparecendo como função ao utilizar o map(), e estava retornando todo o objeto do agente.
