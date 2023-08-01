5e7528c7d609a883e6ad5775e2f73ab02486bbca
henriqueAvner <avnerhdpb@gmail.com>
2023-08-01 18:06:07 -0300

    Feat: Realizando a função getAgentOrbs

--- valorant_functions/getAgentOrbs.js
+++ valorant_functions/getAgentOrbs.js
@@ -1,1 +1,13 @@
-const objAgents = require('../valorant_functions/agents');
+const agents = require('../valorant_functions/agents');
+
+const getAgentOrbs = (orbes) => {
+    if (orbes !== 6 && orbes !== 7 && orbes !== 8) {
+        return "Digite um número válido de orbes (entre 6 e 8)!";
+    }
+    return agents.filter(agent => {
+        if (agent.orbes === orbes) {
+            return agent.name;
+        }
+    }).map(name => name.name);
+}
+console.log(getAgentOrbs(6));
