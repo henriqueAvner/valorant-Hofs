ab80abd957820e301ab21a59e5568fc77ffa6c14
henriqueAvner <avnerhdpb@gmail.com>
2023-08-01 16:41:39 -0300

    Feat: Realizando a função getAlfabeticAgents

--- valorant_functions/getAlfabeticAgents.js
+++ valorant_functions/getAlfabeticAgents.js
@@ -1,1 +1,10 @@
-const objAgents = require('../valorant_functions/agents');
+const agents = require('../valorant_functions/agents');
+
+const getAlfabeticAgents = () => {
+   return agents.filter((agent) => {
+        return agent;
+    }).map((nome) => {
+        return nome.name;
+    }).sort((a, b) => a.localeCompare(b));
+}
+console.log(getAlfabeticAgents());
