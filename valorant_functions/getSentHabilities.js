cc856b2d416baeaa6dbb158e3331443a927a5596
henriqueAvner <avnerhdpb@gmail.com>
2023-08-04 17:45:41 -0300

    Fixed: Refatorando função getSentHabilities

--- valorant_functions/getSentHabilities.js
+++ valorant_functions/getSentHabilities.js
@@ -1,12 +1,9 @@
 const agents = require('../valorant_functions/agents');
 
-const getSentHabilities = () => {
-    return agents.filter((agent) => {
-        if (agent.classe === 'Sentinela') {
-            return agent;
-        }
-    }).map((agente) => {
+const getSentHabilities = () => agents
+    .filter((agent) => agent.classe === 'Sentinela'? agent: '')
+    .map((agente) => {
         return {nome: agente.name, habilidades: agente.habilidades} 
     });
-}
+
 module.exports = getSentHabilities;
